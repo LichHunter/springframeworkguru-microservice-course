bcd37f400bb41e9a72e423accd4d2dd9446733ec
LichHunter <alexander0derevianko@gmail.com>
2022-09-22 16:26:56 +0200

    Added validation for BeerDto

--- mssc-beer-service/src/main/java/spring/course/mssc/beer/service/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/spring/course/mssc/beer/service/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package spring.course.mssc.beer.service.web.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +18,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @Positive
+    @NotNull
     private Long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
 }
